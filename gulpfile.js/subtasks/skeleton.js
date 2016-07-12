2fb494d589f58fb746f9fb70115ec595e42e5c47
Paweł Derehajło <derehajlo@gmail.com>
2016-07-12 09:36:07 +0100

    added skeleton flag in config

--- gulpfile.js/subtasks/skeleton.js
+++ gulpfile.js/subtasks/skeleton.js
@@ -1,5 +1,5 @@
 var skeletonTask = function () {
-    if(!fs.existsSync(config.path.from)) {
+    if(config.skeleton && !fs.existsSync(config.path.from)) {
         return gulp.src(['./gulpfile.js/skeleton/**/*'])
             .pipe($.plumber({errorHandler: onError}))
             .pipe(gulp.dest(config.path.from));
