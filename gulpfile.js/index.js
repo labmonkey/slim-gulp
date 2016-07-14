576b528ca4af3715d32d8b1d022c270c6acee92d
Paweł Derehajło <derehajlo@gmail.com>
2016-07-14 08:20:29 +0100

    fixed custom config merging

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,7 +2,7 @@ var requireDir = require('require-dir');
 
 global.gulp = require('gulp');
 global.fs = require('fs');
-global.config = Object.assign(require('./config'), fs.existsSync('../gulp') ? require('../gulp') : {});
+global.config = Object.assign(require('./config'), fs.existsSync('./gulp.json') ? require('../gulp') : {});
 global.$ = require('gulp-load-plugins')({pattern: config.plugins['gulp-load-plugins'].pattern});
 global.onError = function (error) {
     $.notify.onError({
