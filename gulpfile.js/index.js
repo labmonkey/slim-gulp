c66e12f09a435bf1becb86f8290fc3dadc6d6207
Paweł Derehajło <derehajlo@gmail.com>
2017-06-22 22:32:31 +0100

    added possibility to set --production flag

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -3,8 +3,12 @@ require('./mixins.js');
 global.requireDir = require('require-dir');
 global.gulp = require('gulp');
 global.fs = require('fs');
-global.config = getConfig();
+global.config = loadConfig();
 global.$ = require('gulp-load-plugins')({pattern: config.plugins['gulp-load-plugins'].pattern});
+global.onError = onErrorCallback;
+global.config = loadParams();
 
 requireDir('./tasks');
 requireDir('./subtasks');
+
+console.log(global.config);
--- gulpfile.js/mixins.js
+++ gulpfile.js/mixins.js
@@ -2,23 +2,27 @@ var deepExtend = require('deep-extend');
 
 function onErrorCallback(error) {
     $.notify.onError({
-        title: "Gulp",
-        subtitle: "Failure!",
-        message: "Error: <%= error.message %>",
-        sound: "Beep"
+        title: "Error!",
+        message: "\r\n<%= error.stack %>",
+        sound: "Beep",
+        emitError: true
     })(error);
-
-    $.util.log(error);
+    //$.util.log(error);
 
     this.emit('end');
 }
 
-function getConfig() {
+function loadConfig() {
     var defaultConfig = require('./config');
     var customConfig = fs.existsSync('./gulp.json') ? require('../gulp') : {};
 
     return deepExtend(defaultConfig, customConfig);
 }
 
-global.onError = onErrorCallback;
-global.getConfig = getConfig;
+function loadParams() {
+    return deepExtend(global.config, {"production": !!$.util.env.production});
+}
+
+global.onErrorCallback = onErrorCallback;
+global.loadConfig = loadConfig;
+global.loadParams = loadParams;
